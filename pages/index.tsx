b6670de7a91a35294abc6fb68caa336cf9b37082
Alexis Comte <alexiscomte@MacBook-Pro-de-Alexis.local>
2021-01-10 17:26:46 +0100

    Initialisation projet

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+export default function Home() {
+    return (
+        <div className="container">
+            <h1>Début projet</h1>
+        </div>
+    )
+}
